bc45f387d7d50a264676b88c2e8dd8cf29157751
Casey Richardson <casey@deepwinter.io>
2022-08-21 16:29:23 -0500

    Deployment updates

--- weighbot/urls.py
+++ weighbot/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
 
 # For simplicity, just serve these always for now
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
