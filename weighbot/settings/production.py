71c019fcdf50231e871aa5f6e025685b41317d83
Casey Richardson <casey@deepwinter.io>
2022-08-21 19:31:05 -0500

    Production SECRET_KEY

--- weighbot/settings/production.py
+++ weighbot/settings/production.py
@@ -2,6 +2,8 @@
 import os
 from pathlib import Path
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
